0f25ce317df5e1a46581cd4e0b82f180dafbbf82
Rosa <rosarenee@att.net>
2021-05-19 09:18:26 -0500

    NOT Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun(sleep = "roller-skate") {
+   return `This Saturday, I want to ${sleep}!`
+}
+
+function mondayWork(ew ="go to the office"){
+    return `This Monday, I will ${ew}.`
+}
+
+function wrapAdjective(wow = "special!"){
+//const idkAnymore = wrapAdjective()
+    const reTry = `You are `
+    return function(){
+        let wrap = "*"
+        return function (){
+            return reTry, wrap, wow, wrap 
+        }
+    }
+
+}
+
