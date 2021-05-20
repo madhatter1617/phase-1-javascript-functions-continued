72302e305435ec9a1f46e07d7fd26d174356c640
Rosa <rosarenee@att.net>
2021-05-19 20:25:19 -0500

    stuck on wrapAdjective

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function saturdayFun(sleep = "roller-skate") {
    return `This Saturday, I want to ${sleep}!`
 }
 
-function mondayWork(ew ="go to the office"){
-    return `This Monday, I will ${ew}.`
-}
-
-function wrapAdjective(wrap = "*", wow = "special"){
-    let result = wrapAdjective()
-    let emphatic = result("a hard worker")
-    return function()
-    {
-        const you= "You are ";
-        return function () {
-            return (you, wrapAdjective(wrap), emphatic, wrapAdjective(wow))
-    }
-
-}}
+ function mondayWork(ew ="go to the office"){
+   return `This Monday, I will ${ew}.`
+ }
+ 
+//  function wrapAdjective(wow = "*"){
+//    const part1 = `You are `
+//    return function(){
+//       const part2 = "!"
+//       return function(){
+//       return (part1)
+//       }
+//    }
+   
+  
+ }
 
