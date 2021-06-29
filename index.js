2a8228496fdcbc92cd708838937b3887a01a89bb
Rosa <rosarenee@att.net>
2021-06-29 10:05:02 -0500

    commit tree a38abfd6bb4cecc038e6fa6adfa314976a507583

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 // Your code here
-function saturdayFun(sleep = "roller-skate") {
-   return `This Saturday, I want to ${sleep}!`
+
+function saturdayFun (sleep = "roller-skate"){
+return `This Saturday, I want to ${sleep}!`
+
 }
 
- function mondayWork(ew ="go to the office"){
-   return `This Monday, I will ${ew}.`
- }
- 
- function wrapAdjective(wow = '*'){
-       
-    return function (result = 'a hard worker')
-    { 
-      return `You are ${wow}${result}${wow}!` 
-  }
+function mondayWork(work ="go to the office"){
+  return `This Monday, I will ${work}.`
+
 }
+
+function wrapAdjective(ok = '*'){
+  return function (result = 'a hard worker')
+{
+return `You are ${ok}${result}${ok}!`
+
+}
+}
+
+
+
+
+
+
+// function saturdayFun(sleep = "roller-skate") {
+//    return `This Saturday, I want to ${sleep}!`
+// }
+
+//  function mondayWork(ew ="go to the office"){
+//    return `This Monday, I will ${ew}.`
+//  }
+ 
+//  function wrapAdjective(wow = '*'){
+       
+//     return function (result = 'a hard worker')
+//     { 
+//       return `You are ${wow}${result}${wow}!` 
+//   }
+// }
 
    
    
